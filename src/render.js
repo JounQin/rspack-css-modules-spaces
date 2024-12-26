4d081a81dafb914b63d3800e06cc3bb62032224c
JounQin <admin@1stg.me>
2024-12-26 23:19:16 +0800

    chore: reproduce rsbuild issue

--- src/render.js
+++ src/render.js
@@ -1,8 +1,8 @@
-import './render.css'
+import * as styles from './space folder/render.module.css';
 
 export function render() {
-    const el = document.createElement('div')
-    el.classList.add('text')
-    document.getElementsByTagName('body')[0].appendChild(el)
-    el.innerHTML = 'hello, world'
+  const el = document.createElement('div');
+  el.className = styles.text;
+  document.getElementsByTagName('body')[0].appendChild(el);
+  el.innerHTML = 'hello, world';
 }
